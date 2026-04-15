3a5f7fd88357a4f67192c306bbfec2d0a7327375
Arikh Bin Eden <155133119+Arikh24@users.noreply.github.com>
2026-04-15 14:27:42 +0600

    FinalLabTask2 Login Paze

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if(isset($_POST['submit']))
+    {
+        $username =$_POST['username'];
+        $password =$_POST['password'];
+        if($username == "" || $password == "")
+            {
+                echo "Fillup The Fields";
+            }
+             elseif(!isset($_SESSION['users'][$username.'_password'])) {
+        echo "User not found. Please register first.";
+    }
+    elseif($_SESSION['users'][$username.'_password'] !== $password) {
+        echo "Invalid password!";
+    }
+    else {
+        $_SESSION['loggedin'] = $username;
+        echo "Login successful! Welcome, ".$username;
+        echo "<br><a href='home.html'>Go to home paze</a>";
+    }
+
+?>
+<form method="post">
+    Username :<input type ="text" name="username"><br>
+    Password :<input type ="password" name="password"><br>
+    <input type="submit" name="submit" value="Login">
+</form>
